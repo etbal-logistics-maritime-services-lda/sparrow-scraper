f70b8a4e205dba2713234930b17b7db2ebbc49c5
RadeElias93 <RadeElias93@hotmail.com>
2025-12-27 03:34:20 +0000

    Add Main class with data scraping functionality

---
+++ src/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Scraper scraper = new Scraper();
+        String url = "https://example.com";
+        scraper.scrapeData(url);
+    }
+}
